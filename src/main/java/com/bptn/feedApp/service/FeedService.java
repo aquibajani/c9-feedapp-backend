e60281f256662da45ee402f7b704df0a817587b0
aquibajani <aquib.ajani@obsidi.com>
2026-04-14 11:33:47 -0400

    Code till 2-48 Delete Feed

--- src/main/java/com/bptn/feedApp/service/FeedService.java
+++ src/main/java/com/bptn/feedApp/service/FeedService.java
@@ -22,6 +22,7 @@ import com.bptn.feedApp.domain.PageResponse;
 import java.util.Optional;
 import com.bptn.feedApp.exception.domain.LikeExistException;
 import com.bptn.feedApp.jpa.FeedMetaData;
+import com.bptn.feedApp.exception.domain.FeedNotUserException;
 
 @Service
 public class FeedService {
@@ -116,5 +117,18 @@ public class FeedService {
 	    }
 	        
 	    return this.feedMetaDataRepository.save(newMeta);
+	}
+	
+	public void deleteFeed(int feedId) {
+        
+	    String username = SecurityContextHolder.getContext().getAuthentication().getName();
+	    
+	    Feed feed = this.feedRepository.findById(feedId)            
+	                 .orElseThrow(()-> new FeedNotFoundException(String.format("Feed doesn't exist, %d", feedId)));
+
+	    Optional.of(feed).filter(f -> f.getUser().getUsername().equals(username))
+	                 .orElseThrow(()-> new FeedNotUserException(String.format("Feed doesn't belong to current User, feedId: %d, username: %s", feedId, username)));
+	        
+	    this.feedRepository.delete(feed);
 	}
 }
